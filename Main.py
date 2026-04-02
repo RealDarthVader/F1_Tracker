6ffeee3b76617a23419756545fabce4072c377d8
Utkarsh Gupta <utkarshgupta.vk@gmail.com>
2026-04-02 23:07:06 +0530

    day 5 - code cleanup and reorganise

--- Main.py
+++ Main.py
@@ -1,10 +1,13 @@
-from fastapi import FastAPI
 from fastapi import FastAPI, HTTPException
+
 app = FastAPI()
+
 
 @app.get("/health")
 def health_check():
     return {"status": "ok", "project": "F1 Tracker"}
+
+
 
 @app.get("/races")
 def get_races():
@@ -13,25 +16,7 @@ def get_races():
         {"id": 2, "name": "Saudi Arabian Grand Prix", "season": 2024, "round": 2},
         {"id": 3, "name": "Australian Grand Prix", "season": 2024, "round": 3},
     ]
-@app.get("/drivers")
-def get_drivers():
-    return [
-        {"id": 1, "name": "Max Verstappen", "team": "Red Bull", "number": 1},
-        {"id": 2, "name": "Lewis Hamilton", "team": "Ferrari", "number": 44},
-        {"id": 3, "name": "Lando Norris", "team": "McLaren", "number": 4},
-    ]
 
-@app.get("/drivers/{driver_id}")
-def get_driver(driver_id: int):
-    drivers = [
-        {"id": 1, "name": "Max Verstappen", "team": "Red Bull", "number": 1},
-        {"id": 2, "name": "Lewis Hamilton", "team": "Ferrari", "number": 44},
-        {"id": 3, "name": "Lando Norris", "team": "McLaren", "number": 4},
-    ]
-    for driver in drivers:
-        if driver["id"] == driver_id:
-            return driver
-    return {"error": "Driver not found"}
 @app.get("/races/season")
 def get_races_by_season(year: int):
     all_races = [
@@ -40,6 +25,17 @@ def get_races_by_season(year: int):
         {"id": 3, "name": "Australian Grand Prix", "season": 2024, "round": 3},
     ]
     return [r for r in all_races if r["season"] == year]
+
+
+
+@app.get("/drivers")
+def get_drivers():
+    return [
+        {"id": 1, "name": "Max Verstappen", "team": "Red Bull", "number": 1},
+        {"id": 2, "name": "Lewis Hamilton", "team": "Ferrari", "number": 44},
+        {"id": 3, "name": "Lando Norris", "team": "McLaren", "number": 4},
+    ]
+
 @app.get("/drivers/{driver_id}")
 def get_driver(driver_id: int):
     drivers = [
